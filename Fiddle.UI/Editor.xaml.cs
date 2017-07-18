65709e19d8c447beee238013de2d23968cd1dd4e
mrousavy <marcrousavy@hotmail.com>
2017-07-18 20:23:30 +0200

    UI improvements

--- Fiddle.UI/Editor.xaml.cs
+++ Fiddle.UI/Editor.xaml.cs
@@ -1,132 +1,147 @@
 ﻿using Fiddle.Compilers;
+using System;
 using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
 
-namespace Fiddle.UI
-{
+namespace Fiddle.UI {
     /// <summary>
     /// Interaction logic for Editor.xaml
     /// </summary>
-    public partial class Editor
-    {
+    public partial class Editor {
         private ICompiler Compiler { get; set; }
 
         private string SourceCode => TextBoxCode.Text;
 
-        public Editor()
-        {
+        public Editor() {
             InitializeComponent();
             LoadComboBox();
             LoadTextBox();
+            LoadPreferences();
             TextBoxCode.Focus();
         }
 
-        private void Window_Loaded(object sender, RoutedEventArgs e)
-        {
-            LabelStatusMessage.Content = "Ready";
+        private void LoadPreferences() {
+            Width = App.Preferences.WindowWidth;
+            Height = App.Preferences.WindowHeight;
+            Left = App.Preferences.WindowLeft;
+            Top = App.Preferences.WindowTop;
+            WindowState = App.Preferences.WindowState;
+            TextBoxCode.Text = App.Preferences.SourceCode;
+            TextBoxCode.TextArea.Caret.Offset = App.Preferences.CursorOffset;
+            TextBoxCode.TextArea.Caret.BringCaretToView();
         }
 
-        private void LoadComboBox()
-        {
+        private void LoadComboBox() {
             if (App.Preferences.SelectedLanguage != -1)
                 ComboBoxLanguage.SelectedIndex = App.Preferences.SelectedLanguage;
         }
 
-        private void LoadTextBox()
-        {
-            DataObject.AddCopyingHandler(TextBoxCode, (s, e) =>
-            {
+        private void LoadTextBox() {
+            DataObject.AddCopyingHandler(TextBoxCode, (s, e) => {
                 if (e.IsDragDrop) e.CancelCommand();
             });
-            TextBoxCode.PreviewMouseLeftButtonDown += (s, e) =>
-            {
+            TextBoxCode.PreviewMouseLeftButtonDown += (s, e) => {
                 TextBoxCode.Select(0, 0);
             };
         }
 
-        private async void ButtonCompile(object sender, RoutedEventArgs e)
-        {
+        private void Window_Loaded(object sender, RoutedEventArgs e) {
+            LabelStatusMessage.Content = "Ready";
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e) {
+            LockUi();
+            App.Preferences.WindowWidth = Width;
+            App.Preferences.WindowHeight = Height;
+            App.Preferences.WindowLeft = Left;
+            App.Preferences.WindowTop = Top;
+            App.Preferences.WindowState = WindowState;
+            App.Preferences.SourceCode = SourceCode;
+            App.Preferences.CursorOffset = TextBoxCode.TextArea.Caret.Offset;
+        }
+
+        private async void ButtonCompile(object sender, RoutedEventArgs e) {
             LabelStatusMessage.Content = "Compiling..";
 
             LockUi();
-            Compiler.SourceCode = SourceCode;
-            ICompileResult result = await Compiler.Compile();
+            try {
+                Compiler.SourceCode = SourceCode;
+                ICompileResult result = await Compiler.Compile();
+
+                LabelCompileTime.Content = $"C: {result.Time}ms";
+
+                if (result.Success) {
+                    LabelStatusMessage.Content = "Compilation successful!";
+                } else {
+                    LabelStatusMessage.Content = "Compilation failed!";
+                    await DialogHelper.ShowErrorDialog($"Compilation failed!\n{result.Errors.First()}",
+                        EditorDialogHost);
+                }
+            } catch (Exception ex) {
+                await DialogHelper.ShowErrorDialog($"Could not compile! ({ex.Message})", EditorDialogHost);
+            }
+
             UnlockUi();
-
-            LabelCompileTime.Content = $"C: {result.Time}ms";
-
-            if (result.Success)
-            {
-                LabelStatusMessage.Content = "Compilation successful!";
-            }
-            else
-            {
-                LabelStatusMessage.Content = "Compilation failed!";
-                await DialogHelper.ShowErrorDialog($"Compilation failed!\n{result.Errors.First()}", EditorDialogHost);
-            }
         }
-        private async void ButtonExecute(object sender, RoutedEventArgs e)
-        {
+        private async void ButtonExecute(object sender, RoutedEventArgs e) {
             LabelStatusMessage.Content = "Executing..";
 
             LockUi();
-            Compiler.SourceCode = SourceCode;
-            IExecuteResult result = await Compiler.Execute();
+            try {
+                Compiler.SourceCode = SourceCode;
+                IExecuteResult result = await Compiler.Execute();
+
+                LabelCompileTime.Content = $"C: {result.CompileResult.Time}ms";
+                LabelExecuteTime.Content = $"X: {result.Time}ms";
+
+                if (result.Success) {
+                    LabelStatusMessage.Content = "Execution successful!";
+                } else {
+                    LabelStatusMessage.Content = "Execution failed!";
+                    await DialogHelper.ShowErrorDialog($"Execution failed!\n{result.Exception.Message}", EditorDialogHost);
+                }
+            } catch (Exception ex) {
+                await DialogHelper.ShowErrorDialog($"Could not execute! ({ex.Message})", EditorDialogHost);
+            }
             UnlockUi();
-
-            LabelCompileTime.Content = $"C: {result.CompileResult.Time}ms";
-            LabelExecuteTime.Content = $"X: {result.Time}ms";
-
-            if (result.Success)
-            {
-                LabelStatusMessage.Content = "Execution successful!";
-            }
-            else
-            {
-                LabelStatusMessage.Content = "Execution failed!";
-                await DialogHelper.ShowErrorDialog($"Execution failed!\n{result.Exception.Message}", EditorDialogHost);
-            }
         }
 
-        private void ButtonSave(object sender, RoutedEventArgs e)
-        {
+        private void ButtonSave(object sender, RoutedEventArgs e) {
             LockUi();
             Helper.SaveFile(SourceCode, Compiler.Language);
             UnlockUi();
 
         }
 
-        private void ComboBoxLanguageSelected(object sender, SelectionChangedEventArgs e)
-        {
+        private async void ComboBoxLanguageSelected(object sender, SelectionChangedEventArgs e) {
             LockUi();
             string value = ((ComboBoxItem)ComboBoxLanguage.SelectedValue).Content as string;
-            Compiler = Helper.ChangeLanguage(value, SourceCode, TextBoxCode);
-            App.Preferences.SelectedLanguage = ComboBoxLanguage.SelectedIndex;
-            Title = $"Fiddle - {value}";
+            try {
+                Compiler = Helper.ChangeLanguage(value, SourceCode, TextBoxCode);
+                App.Preferences.SelectedLanguage = ComboBoxLanguage.SelectedIndex;
+                Title = $"Fiddle - {value}";
+            } catch (Exception ex) {
+                await DialogHelper.ShowErrorDialog($"Could not load {value} compiler! ({ex.Message})", EditorDialogHost);
+            }
             UnlockUi();
         }
-        private void TextBoxCode_KeyDown(object sender, KeyEventArgs e)
-        {
+        private void TextBoxCode_KeyDown(object sender, KeyEventArgs e) {
             LabelExecuteTime.Content = string.Empty;
             LabelCompileTime.Content = string.Empty;
             LabelStatusMessage.Content = "Ready";
         }
 
-        private void LockUi()
-        {
-            foreach (UIElement element in EditorGrid.Children)
-            {
+        private void LockUi() {
+            foreach (UIElement element in EditorGrid.Children) {
                 element.IsEnabled = false;
             }
             Cursor = Cursors.Wait;
         }
 
-        private void UnlockUi()
-        {
-            foreach (UIElement element in EditorGrid.Children)
-            {
+        private void UnlockUi() {
+            foreach (UIElement element in EditorGrid.Children) {
                 element.IsEnabled = true;
             }
             Cursor = Cursors.Arrow;
--- Fiddle.UI/Preferences.cs
+++ Fiddle.UI/Preferences.cs
@@ -1,28 +1,31 @@
 ﻿using Newtonsoft.Json;
 using System;
 using System.IO;
+using System.Windows;
 
-namespace Fiddle.UI
-{
-    public class Preferences
-    {
+namespace Fiddle.UI {
+    public class Preferences {
         public int SelectedLanguage { get; set; } = -1;
+
+        public double WindowWidth { get; set; } = 1000;
+        public double WindowHeight { get; set; } = 700;
+        public double WindowLeft { get; set; } = 100;
+        public double WindowTop { get; set; } = 100;
+        public WindowState WindowState { get; set; } = WindowState.Normal;
+        public string SourceCode { get; set; } = "";
+        public int CursorOffset { get; set; } = 0;
     }
 
-    public static class PreferencesManager
-    {
+    public static class PreferencesManager {
         public static string AppData { get; } = Path.Combine(
             Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "Fiddle");
         public static string PreferencesFile { get; set; } = Path.Combine(AppData, "Preferences.json");
 
-        public static Preferences Load()
-        {
-            if (!Directory.Exists(AppData))
-            {
+        public static Preferences Load() {
+            if (!Directory.Exists(AppData)) {
                 Directory.CreateDirectory(AppData);
             }
-            if (!File.Exists(PreferencesFile))
-            {
+            if (!File.Exists(PreferencesFile)) {
                 File.WriteAllText(PreferencesFile, JsonConvert.SerializeObject(new Preferences()));
             }
 
@@ -30,10 +33,8 @@ namespace Fiddle.UI
             return JsonConvert.DeserializeObject<Preferences>(content);
         }
 
-        public static void WriteOut(Preferences prefs)
-        {
-            if (!Directory.Exists(AppData))
-            {
+        public static void WriteOut(Preferences prefs) {
+            if (!Directory.Exists(AppData)) {
                 Directory.CreateDirectory(AppData);
             }
             string content = JsonConvert.SerializeObject(prefs);
