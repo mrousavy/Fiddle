5dc73827b93dae8a1df169433894df7df1f41d30
Marc Rousavy <mrousavy@users.noreply.github.com>
2017-07-21 14:29:37 +0200

    Added Close before Dialog Show

--- Fiddle.UI/DialogHelper.cs
+++ Fiddle.UI/DialogHelper.cs
@@ -5,12 +5,18 @@ using MaterialDesignThemes.Wpf;
 namespace Fiddle.UI {
     public static class DialogHelper {
         public static async Task<bool> ShowYesNoDialog(string question, DialogHost host) {
+            CloseDialog(host);
             object result = await host.ShowDialog(new YesNoDialog(question).GetContent());
             return true;
         }
 
         public static async Task ShowErrorDialog(string message, DialogHost host) {
+            CloseDialog(host);
             await host.ShowDialog(new ErrorDialog(message).GetContent());
+        }
+
+        public static void CloseDialog(DialogHost host) {
+            DialogHost.CloseDialogCommand.Execute(null, host);
         }
     }
 }
