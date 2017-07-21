8b535cb2d46c6fe5d7af54a7b4c176e37cada7bd
Marc Rousavy <mrousavy@users.noreply.github.com>
2017-07-21 16:07:24 +0200

    Highlighting for Diagnostics and bugfixes

--- Fiddle.UI/Helper.cs
+++ Fiddle.UI/Helper.cs
@@ -12,10 +12,14 @@ using System.Windows.Documents;
 using System.Windows.Media;
 using System.Xml;
 
-namespace Fiddle.UI {
-    public static class Helper {
-        public static ICompiler ChangeLanguage(string language, string sourceCode, TextEditor editor) {
-            switch (language) {
+namespace Fiddle.UI
+{
+    public static class Helper
+    {
+        public static ICompiler ChangeLanguage(string language, string sourceCode, TextEditor editor)
+        {
+            switch (language)
+            {
                 case "C#":
                     editor.SyntaxHighlighting = LoadXshd("CSharp.xshd");
                     return Host.NewCompiler(Language.CSharp, sourceCode);
@@ -41,21 +45,26 @@ namespace Fiddle.UI {
         }
 
 
-        private static IHighlightingDefinition LoadXshd(string resourceName) {
+        private static IHighlightingDefinition LoadXshd(string resourceName)
+        {
             Type type = typeof(Helper);
             string fullName = $"{type.Namespace}.Syntax.{resourceName}";
-            using (Stream stream = type.Assembly.GetManifestResourceStream(fullName)) {
+            using (Stream stream = type.Assembly.GetManifestResourceStream(fullName))
+            {
                 if (stream == null)
                     return null;
-                using (XmlTextReader reader = new XmlTextReader(stream)) {
+                using (XmlTextReader reader = new XmlTextReader(stream))
+                {
                     return HighlightingLoader.Load(reader, HighlightingManager.Instance);
                 }
             }
         }
 
 
-        public static string SaveFile(string code, Language language) {
-            SaveFileDialog dialog = new SaveFileDialog {
+        public static string SaveFile(string code, Language language)
+        {
+            SaveFileDialog dialog = new SaveFileDialog
+            {
                 Filter = GetFilterForLanguage(language),
                 FilterIndex = 1,
                 InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.Desktop)
@@ -66,8 +75,10 @@ namespace Fiddle.UI {
             return dialog.FileName;
         }
 
-        private static string GetFilterForLanguage(Language language) {
-            switch (language) {
+        private static string GetFilterForLanguage(Language language)
+        {
+            switch (language)
+            {
                 case Language.Cpp:
                     return "C++ source files (*.cpp)|*.cpp|All files (*.*)|*.*";
                 case Language.CSharp:
@@ -86,13 +97,15 @@ namespace Fiddle.UI {
         }
 
 
-        public static string ConcatErrors(IEnumerable<Exception> errorsList) {
+        public static string ConcatErrors(IEnumerable<Exception> errorsList)
+        {
             string errors = "";
             const int maxErrors = 7; //do not show more than [maxErrors] errors in Message
             int countErrors = 0;
 
             IEnumerable<Exception> exceptions = errorsList as Exception[] ?? errorsList.ToArray(); //kill multiple enums
-            foreach (Exception ex in exceptions) {
+            foreach (Exception ex in exceptions)
+            {
                 errors += $"#{++countErrors}: {ex.Message}{Environment.NewLine}";
 
                 if (countErrors <= maxErrors) continue;
@@ -106,18 +119,43 @@ namespace Fiddle.UI {
             return errors;
         }
 
-        public static string ConcatDiagnostics(IEnumerable<IDiagnostic> diagnosticsList, string indent = "") {
-            int number = 1;
-            return diagnosticsList
-                .Aggregate("",
-                    (current, diagnostic) => current + $"{indent}#{number++}: {diagnostic}{Environment.NewLine}");
+        public static IEnumerable<Run> BuildDiagnostics(IEnumerable<IDiagnostic> diagnostics, string indent = "")
+        {
+            IList<Run> items = new List<Run>();
+            int counter = 1;
+            string nl = Environment.NewLine;
+            foreach (IDiagnostic diagnostic in diagnostics)
+            {
+                Brush brush;
+                switch (diagnostic.Severity)
+                {
+                    case Severity.Error:
+                        brush = Brushes.Red;
+                        break;
+                    case Severity.Warning:
+                        brush = Brushes.Yellow;
+                        break;
+                    default:
+                        brush = Brushes.LightGray;
+                        break;
+                }
+                string lines = diagnostic.LineFrom != diagnostic.LineTo
+                    ? $"{diagnostic.LineFrom}-{diagnostic.LineTo}"
+                    : diagnostic.LineFrom.ToString();
+
+                items.Add(new Run($"{indent}#{counter++} Ln{lines}: ") { Foreground = Brushes.LightGray });
+                items.Add(new Run(diagnostic.Message + nl) { Foreground = brush });
+            }
+            return items;
         }
 
-
-        public static IEnumerable<Run> BuildRuns(IExecuteResult result) {
+        public static IEnumerable<Run> BuildRuns(IExecuteResult result)
+        {
             string nl = Environment.NewLine;
-            if (result.Success) {
-                IList<Run> items = new List<Run> {
+            if (result.Success)
+            {
+                //Execute: SUCCESS, Compile: SUCCESS
+                List<Run> items = new List<Run> {
                     new Run($"Execution successful! (Took {result.Time}ms){nl}") {
                         Foreground = Brushes.Green,
                         FontWeight = FontWeights.Bold,
@@ -125,30 +163,47 @@ namespace Fiddle.UI {
                     }
                 };
 
-                if (result.ReturnValue == null) {
+                if (result.ReturnValue == null)
+                {
                     items.Add(new Run($"Return value: /{nl}") { Foreground = Brushes.Gray });
-                } else {
+                }
+                else
+                {
                     Type type = result.ReturnValue.GetType();
                     items.Add(new Run("Return value: "));
                     items.Add(new Run($"({type.Name}) ") { Foreground = Brushes.Orange });
-                    if (type.IsArray) {
+                    if (type.IsArray)
+                    {
                         Array array = (Array)result.ReturnValue;
                         string run = string.Join(", ", array.Cast<object>());
                         items.Add(new Run($"{run}{nl}") { Foreground = Brushes.CadetBlue });
-                    } else {
+                    }
+                    else
+                    {
                         items.Add(new Run($"{result.ReturnValue}{nl}") { Foreground = Brushes.CadetBlue });
                     }
                 }
-                if (string.IsNullOrWhiteSpace(result.ConsoleOutput)) {
+                if (string.IsNullOrWhiteSpace(result.ConsoleOutput))
+                {
                     items.Add(new Run($"Console output: /{nl}") { Foreground = Brushes.Gray });
-                } else {
+                }
+                else
+                {
                     items.Add(new Run("Console output: "));
                     items.Add(new Run(result.ConsoleOutput) { Foreground = Brushes.Orange });
                 }
+                if (result.CompileResult.Diagnostics?.Any() == true)
+                {
+                    items.Add(new Run("Diagnostics:\n"));
+                    items.AddRange(BuildDiagnostics(result.CompileResult.Diagnostics, " "));
+                }
                 return items;
             }
-            if (result.CompileResult.Success) {
-                IList<Run> items = new List<Run> {
+
+            if (result.CompileResult.Success)
+            {
+                //Execute: FAIL, Compile: SUCCESS
+                List<Run> items = new List<Run> {
                     new Run($"Execution failed! (Took {result.Time}ms){nl}") {
                         Foreground = Brushes.Red,
                         FontWeight = FontWeights.Bold,
@@ -156,41 +211,54 @@ namespace Fiddle.UI {
                     }
                 };
 
-                if (result.Exception == null) {
+                if (result.Exception == null)
+                {
                     items.Add(new Run($"An unexpected error occured.{nl}") { Foreground = Brushes.Gray });
-                } else {
+                }
+                else
+                {
                     items.Add(new Run($"{result.Exception.GetType().Name}: ") { Foreground = Brushes.OrangeRed });
                     items.Add(new Run($"\"{result.Exception.Message}\"{nl}"));
                 }
+                items.Add(new Run("Diagnostics:\n"));
+                items.AddRange(BuildDiagnostics(result.CompileResult.Diagnostics, " "));
 
                 return items;
             }
+
+            //Execute: FAIL, Compile: FAIL
             return BuildRuns(result.CompileResult);
         }
 
-        public static IEnumerable<Run> BuildRuns(ICompileResult result) {
+        public static IEnumerable<Run> BuildRuns(ICompileResult result)
+        {
             string nl = Environment.NewLine;
-            if (result.Success) {
-                string diagnostics = ConcatDiagnostics(result.Diagnostics, " ");
-                IList<Run> items = new List<Run> {
+            if (result.Success)
+            {
+                //Compile: SUCCESS
+                List<Run> items = new List<Run> {
                     new Run($"Compilation successful! (Took {result.Time}ms){nl}") {
                         Foreground = Brushes.Green,
                         FontWeight = FontWeights.Bold,
                         FontSize = 15
                     },
-                    new Run(diagnostics) {Foreground = Brushes.LightGray}
+                    new Run("Diagnostics:\n")
                 };
+                items.AddRange(BuildDiagnostics(result.Diagnostics, " "));
                 return items;
-            } else {
-                string errors = ConcatDiagnostics(result.Diagnostics.Where(d => d.Severity == Severity.Error), " ");
-                IList<Run> items = new List<Run> {
+            }
+            else
+            {
+                //Compile: FAIL
+                List<Run> items = new List<Run> {
                     new Run($"Compilation failed!{nl}") {
                         Foreground = Brushes.Red,
                         FontWeight = FontWeights.Bold,
                         FontSize = 15
                     },
-                    new Run(errors) {Foreground = Brushes.LightGray} //Add new Run(diagnostics) aswell?
+                    new Run("Diagnostics:\n")
                 };
+                items.AddRange(BuildDiagnostics(result.Diagnostics, " "));
                 return items;
             }
         }
