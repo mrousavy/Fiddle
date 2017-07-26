88666554b5c54a658ca0055ed77a226d10f7b544
Marc Rousavy <mrousavy@users.noreply.github.com>
2017-07-26 10:24:26 +0200

    Bugfix

--- Fiddle.UI/Settings.xaml.cs
+++ Fiddle.UI/Settings.xaml.cs
@@ -94,7 +94,7 @@ namespace Fiddle.UI {
 
         private static bool IsTextAllowed(string text)
         {
-            Regex regex = new Regex("^[0-9]+$"); //regex that matches disallowed text
+            Regex regex = new Regex("[^0-9-]+"); //regex that matches disallowed text
             return !regex.IsMatch(text);
         }
 
