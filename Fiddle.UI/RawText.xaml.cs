9b99a89205f8f787ec46fc59b5374f29d96ed724
Marc Rousavy <mrousavy@users.noreply.github.com>
2017-07-27 16:11:22 +0200

    Applied animation Extension methods

--- Fiddle.UI/RawText.xaml.cs
+++ Fiddle.UI/RawText.xaml.cs
@@ -1,27 +1,17 @@
-﻿using System;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Media.Animation;
+﻿using System.Windows;
 
 namespace Fiddle.UI {
     /// <summary>
     ///     Interaction logic for RawText.xaml
     /// </summary>
-    public partial class RawText : Window {
+    public partial class RawText  {
         public RawText(string text) {
             InitializeComponent();
             Text.Text = text;
         }
 
         private async void ButtonClose(object sender, RoutedEventArgs e) {
-            TaskCompletionSource<bool> tcs = new TaskCompletionSource<bool>();
-
-            DoubleAnimation fadeOut = new DoubleAnimation(1, 0, TimeSpan.FromMilliseconds(200));
-            fadeOut.Completed += delegate { tcs.SetResult(true); };
-            BeginAnimation(OpacityProperty, fadeOut);
-
-            await tcs.Task;
-
+            await this.AnimateAsync(OpacityProperty, 1, 0, 200);
             Close();
         }
 
@@ -29,21 +19,13 @@ namespace Fiddle.UI {
             string filename = Helper.SaveFile(Text.Text);
             if (string.IsNullOrWhiteSpace(filename))
                 return;
-
-            TaskCompletionSource<bool> tcs = new TaskCompletionSource<bool>();
-
-            DoubleAnimation fadeOut = new DoubleAnimation(1, 0, TimeSpan.FromMilliseconds(200));
-            fadeOut.Completed += delegate { tcs.SetResult(true); };
-            BeginAnimation(OpacityProperty, fadeOut);
-
-            await tcs.Task;
-
+            
+            await this.AnimateAsync(OpacityProperty, 1, 0, 200);
             Close();
         }
 
         private void WindowLoaded(object sender, RoutedEventArgs e) {
-            DoubleAnimation fadeIn = new DoubleAnimation(0, 1, TimeSpan.FromMilliseconds(200));
-            BeginAnimation(OpacityProperty, fadeIn);
+            this.Animate(OpacityProperty, 0, 1, 200);
         }
     }
 }
