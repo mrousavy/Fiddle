5a7279698c783c63b96f269e5088ba987b450915
mrousavy <mrousavy@donotreply.github.com>
2017-08-29 14:12:34 +0200

    Create Binding Properties for MVVM

--- Fiddle.UI/Settings.xaml.cs
+++ Fiddle.UI/Settings.xaml.cs
@@ -1,17 +1,115 @@
 ﻿using System;
+using System.ComponentModel;
 using System.Diagnostics;
+using System.Runtime.CompilerServices;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Input;
+using Fiddle.UI.Annotations;
 
 namespace Fiddle.UI {
     /// <summary>
     ///     Interaction logic for Settings.xaml
     /// </summary>
-    public partial class Settings {
+    public partial class Settings : INotifyPropertyChanged {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        #region MVVM Properties
+        private bool _uSettings, _wSize, _wPos, _wState, _lang, _rvSize, _sCode, _cPos;
+        private string _jdkPath, _pyPath;
+        private long _cTimeout, _eTimeout;
+
+        public bool USettings { //Remember any settings at all
+            get => _uSettings;
+            set {
+                _uSettings = value;
+                OnPropertyChanged();
+            }
+        }
+        public bool WSize { //Remember window size
+            get => _wSize;
+            set {
+                _wSize = value;
+                OnPropertyChanged();
+            }
+        }
+        public bool WPos { //Remember Window Position
+            get => _wPos;
+            set {
+                _wPos = value;
+                OnPropertyChanged();
+            }
+        }
+        public bool WState { //Remember Window State
+            get => _wState;
+            set {
+                _wState = value;
+                OnPropertyChanged();
+            }
+        }
+        public bool Lang { //Remember Language
+            get => _lang;
+            set {
+                _lang = value;
+                OnPropertyChanged();
+            }
+        }
+        public bool RvSize { //Remember results view size
+            get => _rvSize;
+            set {
+                _rvSize = value;
+                OnPropertyChanged();
+            }
+        }
+        public bool SCode { //Remember source code
+            get => _sCode;
+            set {
+                _sCode = value;
+                OnPropertyChanged();
+            }
+        }
+        public bool CPos { //Remember cursor position
+            get => _cPos;
+            set {
+                _cPos = value;
+                OnPropertyChanged();
+            }
+        }
+        public string JdkPath { //Path to JDK
+            get => _jdkPath;
+            set {
+                _jdkPath = value;
+                OnPropertyChanged();
+            }
+        }
+        public string PyPath { //Path to Python library
+            get => _pyPath;
+            set {
+                _pyPath = value;
+                OnPropertyChanged();
+            }
+        }
+        public long CTimeout { //Compile timeout
+            get => _cTimeout;
+            set {
+                _cTimeout = value;
+                OnPropertyChanged();
+            }
+        }
+        public long ETimeout { //Execute timeout
+            get => _eTimeout;
+            set {
+                _eTimeout = value;
+                OnPropertyChanged();
+            }
+        }
+        #endregion
+
+
         public Settings() {
             InitializeComponent();
+            DataContext = this;
             Load();
         }
 
@@ -54,26 +152,26 @@ namespace Fiddle.UI {
 
         private async Task Apply() {
             try {
-                App.Preferences.CacheUserSettings = Convert.ToBoolean(USettings.IsChecked);
-                App.Preferences.JdkPath = Jdk.Text;
-                App.Preferences.PyPath = PyPaths.Text;
-                App.Preferences.CompileTimeout = long.Parse(ComTimeout.Text);
-                App.Preferences.ExecuteTimeout = long.Parse(ExTimeout.Text);
+                App.Preferences.CacheUserSettings = Convert.ToBoolean(USettings);
+                App.Preferences.JdkPath = JdkPath;
+                App.Preferences.PyPath = PyPath;
+                App.Preferences.CompileTimeout = CTimeout;
+                App.Preferences.ExecuteTimeout = ETimeout;
 
                 App.Preferences.CacheType = CacheType.Nothing;
-                if (WSize.IsChecked == true)
+                if (WSize)
                     App.Preferences.CacheType |= CacheType.WindowSize;
-                if (WPos.IsChecked == true)
+                if (WPos)
                     App.Preferences.CacheType |= CacheType.WindowPos;
-                if (WState.IsChecked == true)
+                if (WState)
                     App.Preferences.CacheType |= CacheType.WindowState;
-                if (Lang.IsChecked == true)
+                if (Lang)
                     App.Preferences.CacheType |= CacheType.Language;
-                if (RvSize.IsChecked == true)
+                if (RvSize)
                     App.Preferences.CacheType |= CacheType.ResultsViewSize;
-                if (SCode.IsChecked == true)
+                if (SCode)
                     App.Preferences.CacheType |= CacheType.SourceCode;
-                if (CPos.IsChecked == true)
+                if (CPos)
                     App.Preferences.CacheType |= CacheType.CursorPos;
             } catch (Exception ex) {
                 //error converting
@@ -82,19 +180,18 @@ namespace Fiddle.UI {
         }
 
         private void Load() {
-            USettings.IsChecked = App.Preferences.CacheUserSettings;
-            Jdk.Text = App.Preferences.JdkPath;
-            Jdk.Text = App.Preferences.JdkPath;
-            PyPaths.Text = App.Preferences.PyPath;
-            ComTimeout.Text = App.Preferences.CompileTimeout.ToString();
-            ExTimeout.Text = App.Preferences.ExecuteTimeout.ToString();
-            WSize.IsChecked = App.Preferences.CacheType.HasFlag(CacheType.WindowSize);
-            WPos.IsChecked = App.Preferences.CacheType.HasFlag(CacheType.WindowPos);
-            WState.IsChecked = App.Preferences.CacheType.HasFlag(CacheType.WindowState);
-            Lang.IsChecked = App.Preferences.CacheType.HasFlag(CacheType.Language);
-            RvSize.IsChecked = App.Preferences.CacheType.HasFlag(CacheType.ResultsViewSize);
-            SCode.IsChecked = App.Preferences.CacheType.HasFlag(CacheType.SourceCode);
-            CPos.IsChecked = App.Preferences.CacheType.HasFlag(CacheType.CursorPos);
+            USettings = App.Preferences.CacheUserSettings;
+            JdkPath = App.Preferences.JdkPath;
+            PyPath = App.Preferences.PyPath;
+            CTimeout = App.Preferences.CompileTimeout;
+            ETimeout = App.Preferences.ExecuteTimeout;
+            WSize = App.Preferences.CacheType.HasFlag(CacheType.WindowSize);
+            WPos = App.Preferences.CacheType.HasFlag(CacheType.WindowPos);
+            WState = App.Preferences.CacheType.HasFlag(CacheType.WindowState);
+            Lang = App.Preferences.CacheType.HasFlag(CacheType.Language);
+            RvSize = App.Preferences.CacheType.HasFlag(CacheType.ResultsViewSize);
+            SCode = App.Preferences.CacheType.HasFlag(CacheType.SourceCode);
+            CPos = App.Preferences.CacheType.HasFlag(CacheType.CursorPos);
         }
 
 
@@ -110,11 +207,16 @@ namespace Fiddle.UI {
         // Use the DataObject.Pasting Handler 
         private void TextBoxPasting(object sender, DataObjectPastingEventArgs e) {
             if (e.DataObject.GetDataPresent(typeof(string))) {
-                string text = (string) e.DataObject.GetData(typeof(string));
+                string text = (string)e.DataObject.GetData(typeof(string));
                 if (!IsTextAllowed(text)) e.CancelCommand();
             } else {
                 e.CancelCommand();
             }
+        }
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null) {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
     }
 }
